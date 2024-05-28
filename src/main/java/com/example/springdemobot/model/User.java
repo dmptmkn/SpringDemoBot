3d0f2aa5d955b20fe618a201b966771e18b421c8
dmptmkn <dm.ptmkn@yandex.ru>
2024-05-28 22:32:08 +0300

    add User class with annotations

---
+++ src/main/java/com/example/springdemobot/model/User.java
@@ -0,0 +1,26 @@
+package com.example.springdemobot.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@Entity(name = "userDataTable")
+public class User {
+
+    @Id
+    private long chatId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String userName;
+
+    private Timestamp registeredAt;
+
+}
